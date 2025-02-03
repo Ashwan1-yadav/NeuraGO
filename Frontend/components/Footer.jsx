5518d1948a8d7e088aab30c770b079bccd431b7e
Ashwan1-yadav <ashwaniyadav888@gmail.com>
2025-02-03 18:17:34 +0530

    Added Pages and components

---
+++ Frontend/components/Footer.jsx
@@ -0,0 +1,14 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className='footer h-12 p-4 bg-black-10 flex justify-around items-center bg-black text-white text-sm font-sans text-center text-gray-400 absolute inset-x-0 bottom-0'>
+        <p className='text-center text-sm'>
+            Copyright © 2025 Neurago. All rights reserved.
+        </p>
+    </div>
+  )
+}
+
+export default Footer
