7cdc14cace18c00ed28488a063eabdbd9f77ef80
Ashwan1-yadav <ashwaniyadav888@gmail.com>
2025-02-06 15:14:07 +0530

    Created Home UI : User

--- Frontend/pages/Dashboard.jsx
+++ Frontend/pages/Dashboard.jsx
@@ -1,9 +1,12 @@
-/* eslint-disable no-unused-vars */
-import React from 'react'
-
 const Dashboard = () => {
   return (
-    <div>Dashboard</div>
+    <div>
+      <p className="w-18 absolute left-3 top-3 z-10 text-black font-bold  bg-gray-400 rounded-full bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-8 border border-gray-100 text-[16px] text-center text-gray-800 shadow-md">NeuraGO</p>
+      <div className="h-screen w-screen">
+        {/* Navigation Bar */}
+        <img src="https://storage.googleapis.com/support-forums-api/attachment/thread-311121232-12633085076850476787.png" alt="Map Background" className="w-full h-full object-cover" />
+      </div>
+    </div>
   )
 }
 
